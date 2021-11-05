71f0e45a5430fc9992f116634d66d2422e5059c7
Damien Auversack <66826942+DamsOver@users.noreply.github.com>
2021-11-05 15:07:37 +0100

    matchView in progress

--- include/MatchView.h
+++ include/MatchView.h
@@ -1,16 +1,19 @@
 #ifndef MATCHVIEW_H
 #define MATCHVIEW_H
-
+#include <SFML/Graphics.hpp>
+#include <vector>
 
 class MatchView {
     private:
-        //vector<>
+        std::vector<sf::CircleShape> roundCircles;
 
     public:
         MatchView();
         virtual ~MatchView();
         MatchView(const MatchView& other);
 
+        void createRoundCircles();
+        std::vector<sf::CircleShape> getRoundCircles();
 };
 
 #endif // MATCHVIEW_H
