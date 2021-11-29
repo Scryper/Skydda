4090ee2765750b6b7090a586fc44fcb05fbd3883
Damien Auversack <66826942+DamsOver@users.noreply.github.com>
2021-11-29 12:49:31 +0100

    remove size

--- include/SpriteInitializer.h
+++ include/SpriteInitializer.h
@@ -4,23 +4,22 @@
 #include <SFML/Graphics.hpp>
 
 #include "Position.h"
-#include "Size.h"
 #include "CoupleFloat.h"
 #include "TextureLoader.h"
 
-sf::Sprite setSpriteOptions(sf::Sprite *sprite, Size size_, Position position);
+sf::Sprite setSpriteOptions(sf::Sprite *sprite, CoupleFloat size_, Position position);
 
-sf::Sprite setSpriteOptionsPlayer(sf::Sprite *sprite, Size size_, Size centerOfSprite, Position position);
+sf::Sprite setSpriteOptionsPlayer(sf::Sprite *sprite, CoupleFloat size_, CoupleFloat centerOfSprite, Position position);
 
 // Initialize the sprite with created sizeCouple
-sf::Sprite initSprite(Size sizeOfSprite, std::string pathToSprite, Position spritePosition, sf::Texture *spriteTexture);
+sf::Sprite initSprite(CoupleFloat sizeOfSprite, std::string pathToSprite, Position spritePosition, sf::Texture *spriteTexture);
 
-sf::Sprite initSpritePlayer(Size sizeOfSprite, Size centerOfSprite, std::string pathToSprite, Position spritePosition, sf::Texture *spriteTexture);
+sf::Sprite initSpritePlayer(CoupleFloat sizeOfSprite, CoupleFloat centerOfSprite, std::string pathToSprite, Position spritePosition, sf::Texture *spriteTexture);
 
 // Initialize the sprite and creates itself the sizeCouple
 sf::Sprite initSprite(float sizeX, float sizeY, std::string pathToSprite, Position spritePosition, sf::Texture *spriteTexture);
 
-sf::Sprite initSprite(Size sizeOfSprite, Position spritePosition, sf::Texture *spriteTexture);
+sf::Sprite initSprite(CoupleFloat sizeOfSprite, Position spritePosition, sf::Texture *spriteTexture);
 
 // Initialize a sprite which has a texture already loaded
 sf::Sprite initSprite(float sizeX, float sizeY, Position spritePosition, sf::Texture *spriteTexture);
