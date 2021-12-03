1867392b93f5b66173526f6424544157b03b7622
Scryper <la199701@student.helha.be>
2021-12-03 17:41:50 +0100

    can choose the game mode with visual changement

--- include/OptionScreen.h
+++ include/OptionScreen.h
@@ -2,8 +2,35 @@
 #define OPTIONSCREEN_H
 
 #include "Screen.h"
+#include "GlobalVariables.h"
+#include "MusicManager.h"
 
 class OptionScreen : public Screen {
+    private:
+        sf::Font font;
+
+        sf::Text textBack;
+        sf::Text textValueOfVolume;
+
+        sf::Texture textureBackground;
+        sf::Texture textureButton;
+        sf::Texture textureButtonPlusMinus;
+
+        sf::Sprite backgroundSprite;
+        sf::Sprite buttonBack;
+        sf::Sprite buttonPlus;
+        sf::Sprite buttonMinus;
+
+        Position position;
+
+        MusicManager* musicManager = MusicManager::getInstance();
+
+        std::vector<sf::Text*> texts;
+
+        void initFonts();
+        void initBackground();
+        void changeVolume(float value);
+
     public:
         virtual int run(sf::RenderWindow &app, std::vector<std::string> data, int seed);
 };
