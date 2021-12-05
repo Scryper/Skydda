7f9b37d42871ba818cce04981553acf94fcced4f
Scryper <la199701@student.helha.be>
2021-12-05 19:23:31 +0100

    Create the builder files

---
+++ include/PlayerViewBuilder.h
@@ -0,0 +1,18 @@
+#ifndef PLAYERVIEWBUILDER_H
+#define PLAYERVIEWBUILDER_H
+
+
+class PlayerViewBuilder
+{
+    public:
+        PlayerViewBuilder();
+        virtual ~PlayerViewBuilder();
+        PlayerViewBuilder(const PlayerViewBuilder& other);
+        PlayerViewBuilder& operator=(const PlayerViewBuilder& other);
+
+    protected:
+
+    private:
+};
+
+#endif // PLAYERVIEWBUILDER_H
---
+++ src/PlayerViewBuilder.cpp
@@ -0,0 +1,23 @@
+#include "PlayerViewBuilder.h"
+
+PlayerViewBuilder::PlayerViewBuilder()
+{
+    //ctor
+}
+
+PlayerViewBuilder::~PlayerViewBuilder()
+{
+    //dtor
+}
+
+PlayerViewBuilder::PlayerViewBuilder(const PlayerViewBuilder& other)
+{
+    //copy ctor
+}
+
+PlayerViewBuilder& PlayerViewBuilder::operator=(const PlayerViewBuilder& rhs)
+{
+    if (this == &rhs) return *this; // handle self assignment
+    //assignment operator
+    return *this;
+}
