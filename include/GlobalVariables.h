7efb7177edf5a1a5467d1e36275dab951c265b52
Scryper <40074382+Scryper@users.noreply.github.com>
2021-12-07 20:17:26 +0100

    chage volume

--- include/GlobalVariables.h
+++ include/GlobalVariables.h
@@ -7,7 +7,7 @@ Contains various variables used in the code
 class GlobalVariables {
     public:
         //default volume of the music
-        static inline float VOLUME_MUSIC = 5.f;
+        static inline float VOLUME_MUSIC = 50.f;
         //default number of maximum FPS
         static inline int FPS = 60;
 };
