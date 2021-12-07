8c71c00e93ea01ce5f5f1ddbf3b54db0ba32cbcf
Scryper <40074382+Scryper@users.noreply.github.com>
2021-12-07 17:36:18 +0100

    comment singleton music manager

--- src/MusicManager.cpp
+++ src/MusicManager.cpp
@@ -9,12 +9,19 @@ MusicManager::MusicManager() {
     music.setVolume(GlobalVariables::VOLUME_MUSIC);
 }
 
+// Sound manager is not copyable
+// So to be able to use it via the getInstance Method, we send a pointer of the object
+// So the call should be SoundManager* var = SoundManager::getInstance(); 
+// and not SoundManager var = SoundManager::getInstance();
+// As the second option will attempt to make a copy because of "="
+// The ability to make a copy of this singleton object, will break the singleton aspect of this object
+// This is why SoundManager is not copyable
 MusicManager* MusicManager::getInstance() {
-    std::lock_guard<std::mutex> lock(mutex_); // thread safe
-    if(instance == nullptr) {
+    std::lock_guard<std::mutex> lock(mutex_); // allows the singleton to be thread safe
+    if(instance == nullptr) { // if there is no instance of the object
         instance = new MusicManager();
     }
-    return instance;
+    return instance; // return the instance
 }
 
 void MusicManager::play() {
